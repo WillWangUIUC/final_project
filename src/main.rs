ae13b9c533c796b3318cd90aefb9f325ee44ef2e
Zihao Wang <zihaow6@illinois.edu>
2024-12-09 17:09:06 -0600

    call encryption function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+mod chacha20;
+use chacha20::ChaCha20;
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+
 fn main() {
     println!(1);
     let mut chacha = ChaCha20::new_default();
-    let key = [0u8; 32];
+    chacha.encrypt_file("files\input.txt", "files\output.txt")?;
+    Ok(())
 }
