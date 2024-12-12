9c32ef25d45d2f52ef9015bdd6c3d11da734b7cd
Zihao Wang <zihaow6@illinois.edu>
2024-12-11 22:11:28 -0600

    comment

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::fs::File;
 use std::path::Path;
 
 fn main() -> io::Result<()> {
-    println!("{}",1);
+    println!("{}",1); //make sure the program is running
     let mut chacha = ChaCha20::new_default();
     chacha.encrypt_file("files\\input.txt", "files\\output.txt")?;
     chacha.set_counter(0);
