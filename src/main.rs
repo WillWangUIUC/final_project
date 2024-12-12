a699825aa75e38adab3d70299667e78d47ceebea
Zihao Wang <zihaow6@illinois.edu>
2024-12-11 22:17:46 -0600

    main commented

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use std::path::Path;
 fn main() -> io::Result<()> {
     println!("{}",1); //make sure the program is running
     let mut chacha = ChaCha20::new_default();
-    chacha.encrypt_file("files\\input.txt", "files\\output.txt")?;
-    chacha.set_counter(0);
-    chacha.decrypt_file("files\\output.txt", "files\\decrypted.txt")?;
+    chacha.encrypt_file("files\\input.txt", "files\\output.txt")?; //encrypt the file and output the encrypted string to output.txt
+    chacha.set_counter(0); //reset the counter
+    chacha.decrypt_file("files\\output.txt", "files\\decrypted.txt")?; //decrypt the file and output the decrypted string to decrypted.txt
     Ok(())
 }
